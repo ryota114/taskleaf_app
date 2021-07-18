3bec9d0daf76cab99bf31e0e0b75e542f78669b5
ryota114 <namahann0978@gmail.com>
2021-07-18 03:11:52 +0000

    validatesの追加、マイグレーションの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_17_095526) do
+ActiveRecord::Schema.define(version: 2021_07_18_021616) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
