4371640bb56f9e2b84070a7cc08bad609adf8e87
ryota114 <namahann0978@gmail.com>
2021-07-23 09:24:11 +0000

    ユーザー管理機能一部

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,18 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end 
+  
   def new
     @user = User.new
+  end
+  
+  def edit
+    @user = User.find(params[:id])
   end
   
   def create
@@ -12,15 +24,23 @@ class Admin::UsersController < ApplicationController
       render :new
     end 
   end
-
-  def edit
-  end
-
-  def show
-  end
-
-  def index
-  end
+  
+  def update 
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
+    else
+      render :edit
+    end 
+  end 
+  
+  def destroy 
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を削除しました。"
+  end 
+  
   
   private
   
