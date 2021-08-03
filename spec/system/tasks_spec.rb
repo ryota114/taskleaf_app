86a6391d216683c6f0c7c25d704a35753980e25b
ryota114 <namahann0978@gmail.com>
2021-08-03 17:24:33 +0000

    letを使用した共通化

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,26 +2,37 @@ require "rails_helper"
 
 describe "タスク管理機能", type: :system do
   describe "一覧表示機能" do 
+    let(:user_a) { FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com") }
+    let(:user_b) { FactoryBot.create(:user, name: "ユーザーB", email: "b@example.com") }
+    
     before do 
-      # ユーザーA機能を作成しておく
-      user_a = FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com")
       # 作成者がユーザーAであるタスクを作成しておく
       FactoryBot.create(:task, name: "最初のタスク", user: user_a)
+      # ユーザーでログインする
+        visit login_path
+        fill_in "メールアドレス", with: login_user.email
+        fill_in "パスワード", with: login_user.password
+        click_button "ログインする"
     end 
     
     context "ユーザーAがログインしているとき" do 
-      before do
-        # ユーザーAでログインする
-        visit login_path
-        fill_in "メールアドレス", with: "a@example.com"
-        fill_in "パスワード", with: "password"
-        click_button "ログインする"
-      end 
+      let(:login_user) { user_a }
       
-      it "ユーザーAが作成したタスクが表示される表示される" do
+      it "ユーザーAが作成したタスクが表示される" do
         # 作成済みのタスクの名称が画面上に表示されていることを確認
         expect(page).to have_content "最初のタスク"
       end 
     end 
+    
+    context "ユーザーBがログインしているとき" do
+      let(:login_user) { user_b }
+      
+      it "ユーザーAが作成したタスクが表示されない" do
+        # ユーザーAが作成したタスクの名称が画面上に表示されていないことを確認
+        # expect(page).not_to ...でもよい
+        expect(page).to have_no_content "最初のタスク"
+      end 
+    end 
+    
   end 
 end
